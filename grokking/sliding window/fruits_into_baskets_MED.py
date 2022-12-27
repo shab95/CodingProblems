4edd8332b82dcf8beb9224edce82e5cc05e3aa48
shab95 <shabarishnair95@gmail.com>
2022-12-27 16:53:27 -0500

    Fruits into baskets

--- grokking/sliding window/fruits_into_baskets_MED.py
+++ grokking/sliding window/fruits_into_baskets_MED.py
@@ -1,22 +1,44 @@
 def fruitbasket(fruits):
     window_start = 0
-    charHM = {}
-    maxFruits = 0
+    maxCount = 0
+    fruitFrequency = {}
     for window_end in range(len(fruits)):
-        curFruit = fruits[window_end]
-        if curFruit not in charHM:
-            charHM[curFruit] = 0
-        charHM[curFruit] += 1
-  
-        while len(charHM) > 2:
-            leftFruit = fruits[window_start]
-            charHM[leftFruit] -= 1
-            if charHM[leftFruit] == 0:
-                del charHM[leftFruit]
+        fruit = fruits[window_end]
+        if fruit not in fruitFrequency:
+            fruitFrequency[fruit] = 0
+        fruitFrequency[fruit] += 1
+        while len(fruitFrequency) > 2:
+            firstFruit = fruits[window_start]
+            fruitFrequency[firstFruit] -= 1
+            if fruitFrequency[firstFruit] == 0:
+                del fruitFrequency[firstFruit]
             window_start += 1
-        maxFruits = max(maxFruits, window_end - window_start + 1)
-    return maxFruits
+        maxCount = max(maxCount, window_end - window_start + 1)
 
-print(str(fruitbasket(['A','B','C','A','C'])))
-print(str(fruitbasket(['A','B','C','B', 'B','C'])))        
-  
+    return maxCount
+
+
+'''
+Summary:
+The basis of the problem is to find the longest substring of 2 distinct characters in a string.
+
+To solve the problem, we use the sliding window technique. We go through each of the fruits at the beginning of the list until we find
+our third distinct fruit. While adding fruits, we keep count of how many of each fruit we have seen. At the third distinct fruit, we
+remove the fruits we saw at the beginning until there are only 2 distinct fruits in our window again. In each iteration, we check
+if the length of our window(number of fruits we have) is bigger than the previous max.
+
+Time Complexity: O(N) - The outer for loop encounters each character exactly once. The inner while loop encounters each character exactly
+                        once. N + N -> O(N)
+                        
+Space Complexity: O(1) - There's going to be a maximum of 3 fruits inside the hashmap. The extra fruit is in when we encounter the 3rd
+                         distinct fruit.
+'''
+
+
+def main():
+
+    print(str(fruitbasket(['A', 'B', 'C', 'A', 'C'])))
+    print(str(fruitbasket(['A', 'B', 'C', 'B', 'B', 'C'])))
+
+
+main()
