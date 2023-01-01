0e8626b50d26ea64b444bb08bc77a8c02ed91d21
shab95 <shabarishnair95@gmail.com>
2023-01-01 14:10:30 -0500

    middle linkedlist

--- grokking/Fast Slow Pointers/Middle_LinkedList_EASY.py
+++ grokking/Fast Slow Pointers/Middle_LinkedList_EASY.py
@@ -8,9 +8,24 @@ def find_middle_of_linked_list(head):
     slow, fast = head, head
 
     while fast and fast.next:
-        fast = fast.next.next
         slow = slow.next
+        fast = fast.next.next
+
     return slow
+
+
+'''
+Summary:
+
+This problem can be solved using the fast slow pointers technique. The fast pointer always moves two steps in one iteration and the slow
+pointer moves one step in one iteration. That means the fast pointer will always be double the distance the slow pointer has went.
+As a result, it also means that the slow pointer has covered half the distance the fast pointer has. If the fast pointer has traveled
+the entiriety of the linkedlist, then that means the slow pointer is half way through the list and will be pointing at the middle node.
+
+Time Complexity: O(N) - The fast pointer went across the whole list, processing every other element. The slow pointer only covered
+                        half the linkedlist. N/2 + N/2 -> N
+Space Complexity: O(1) - No extra space is needed.
+'''
 
 
 def main():
